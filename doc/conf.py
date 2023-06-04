6808b60b9697724ebe60b76ce4206f71da45ad6c
gormaniac <83027042+gormaniac@users.noreply.github.com>
2023-06-04 15:00:08 -0600

    doc: docs fix

--- doc/conf.py
+++ doc/conf.py
@@ -1,8 +1,8 @@
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('../src'))
 
-project = 'caic_python'
+project = 'caic-python'
 copyright = '2023, John Gorman'
 author = 'John Gorman'
 
