2c2f4626c388373110d7317574fac2fbf75be8f5
gormaniac <83027042+gormaniac@users.noreply.github.com>
2023-06-04 14:44:16 -0600

    build: release v0.1.9

--- doc/conf.py
+++ doc/conf.py
@@ -1,6 +1,6 @@
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../src/caic-python'))
+sys.path.insert(0, os.path.abspath('..'))
 
 project = 'caic-python'
 copyright = '2023, John Gorman'
